3194a12ee6a2f60f9ad556a41ceb510002236d7e
IzaanD98 <izzy.dalvi@gmail.com>
2023-03-30 17:19:45 +0100

    changes to cors 2

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
