9c7861c280835a43346e314b419dad122c101bb6
IzaanD98 <izzy.dalvi@gmail.com>
2023-03-30 17:08:36 +0100

    added cors access

--- server.js
+++ server.js
@@ -30,6 +30,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use(
   session({
     secret: "keyboard cat",
