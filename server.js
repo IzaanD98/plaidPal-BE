511ff5eec161eb44094cc4bbf4b39ee6b0232e28
Michael Greenan <mikegreenan98@gmail.com>
2023-03-31 17:42:17 +0100

    fs

--- server.js
+++ server.js
@@ -42,6 +42,9 @@ app.use(cors());
 //   })
 // );   // Hmmmmmm :D   Deployed, this will be different. 
 
+
+// res.setHeader('Access-Control-Allow-Origin', '*');
+
 app.use((req, res, next) => {
     res.setHeader(
     "Access-Control-Allow-Headers",
@@ -51,7 +54,7 @@ app.use((req, res, next) => {
       "Access-Control-Allow-Methods",
       "GET, POST, PATCH, DELETE, OPTIONS"
     );
-    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Credentials", true);
     next();
   });
   
