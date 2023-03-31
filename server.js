b237d2afc426c6a2f69f73894a50339455140d84
Michael Greenan <mikegreenan98@gmail.com>
2023-03-31 17:36:41 +0100

    sd

--- server.js
+++ server.js
@@ -42,23 +42,20 @@ app.use(cors());
 //   })
 // );   // Hmmmmmm :D   Deployed, this will be different. 
 
-
-
+app.use((req, res, next) => {
+    res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+  
 app.use(express.json());
-
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PATCH, DELETE, OPTIONS"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
-
 app.use(
   session({
     secret: "keyboard cat",
